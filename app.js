c665f52a26ff3d95b5363332e74c550669d004ea
Your Name <yacinebenz3@gmail.com>
2017-08-06 20:00:00 -0600

    added package.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 let res = notes.addNote();
